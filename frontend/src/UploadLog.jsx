6d20d779c4005f30a037d5e564cec88f5b274a92
Anthony joseph achibi <anthonyachibi@gmail.com>
2025-09-09 13:19:13 +0100

    Update UploadLog.jsx

--- frontend/src/UploadLog.jsx
+++ frontend/src/UploadLog.jsx
@@ -32,7 +32,7 @@ const UploadLog = () => {
       setSuccess('');
       setThreats([]);
 
-      const res = await axios.post('http://localhost:5000/api/upload', formData);
+      const res = await axios.post('https://intrusion-detection-system-7.onrender.com/api/upload', formData);
       const detected = res.data.threats || [];
 
       setThreats(detected);

9f7370c8b5800848fd25f316e27d0a3306be3231
Anthony joseph achibi <anthonyachibi@gmail.com>
2025-09-09 12:46:29 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -103,8 +103,8 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 // });
-app.get('/*', (req, res) => {
-  res.sendFile(join(__dirname, '../frontend/dist/index.html'));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
